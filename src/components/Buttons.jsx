14d623b529724b64ca0e057a1dc76339aec228ca
Visar <visarmullaj07@gmail.com>
2024-04-01 21:44:21 +0200

    updated routes/links

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Buttons = ({ width, children, path }) => {
 	return (
-		<Link to={`/levlin/${path}`} className={`${width && width}`}>
+		<Link to={`/Levlin/${path}`} className={`${width && width}`}>
 			<button className="w-full uppercase font-foe text-white  my-3 p-1 border text-3xl border-gray-200 hover:bg-slate-100 hover:text-gray-900 active:bg-white transition-all">
 				{children} &gt;
 			</button>
